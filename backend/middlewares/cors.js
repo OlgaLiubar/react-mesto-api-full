4432a4d70b0b6b6574aead0ec509726686d03d1a
Ольга Любар <olgalubar@MacBook-Air-Olga.local>
2021-07-17 16:52:19 +0300

    cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,21 +1,29 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
   'https://olgaliubar.students.nomoredomains.monster',
+  'http://olgaliubar.students.nomoredomains.monster',
   'http://api.olgaliubar.students.nomoredomains.monster',
+  'localhost:3000',
+  'http://84.201.130.24',
+  'https://84.201.130.24',
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-module.exports = function cors(req, res, next) {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  // const requestHeaders = req.headers['access-control-request-headers'];
+module.exports = (req, res, next) => {
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
 
+  const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
+  }
+
+  if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.status(200).send();
+    return;
   }
 
   next();
